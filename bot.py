287825b5db1d93587d3f349e125e56b12a63619a
panelsale39-wq <panelsale39@gmail.com>
2026-03-16 14:06:29 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,26 @@
-import os
-from telegram.ext import Updater, CommandHandler
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
 
-TOKEN = os.environ.get("8690887958:AAEHfy8T9gH1Tc4sQnI1MDvbr8zo2oXMjfc")
+TOKEN = "8690887958:AAEHfy8T9gH1Tc4sQnI1MDvbr8zo2oXMjfc"
 
-balance_in = 0
-balance_out = 0
+balance = 0
 
-def start(update, context):
-    update.message.reply_text("Bot chal raha hai ✅")
-
-def add(update, context):
-    global balance_in
-    amount = int(context.args[0])
-    balance_in += amount
-    update.message.reply_text(f"You received ${amount}\nTotal In: ${balance_in}")
-
-def out(update, context):
-    global balance_out
-    amount = int(context.args[0])
-    balance_out += amount
-    update.message.reply_text(f"You sent ${amount}\nTotal Out: ${balance_out}")
+def handle_message(update, context):
+    global balance
+    text = update.message.text
+    
+    if "received" in text.lower():
+        words = text.split()
+        for w in words:
+            if w.replace("$","").isdigit():
+                amount = int(w.replace("$",""))
+                balance += amount
+                update.message.reply_text(f"Total Balance: ${balance}")
 
 updater = Updater(TOKEN, use_context=True)
-
 dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-dp.add_handler(CommandHandler("add", add))
-dp.add_handler(CommandHandler("out", out))
+
+dp.add_handler(MessageHandler(Filters.text, handle_message))
 
 updater.start_polling()
 updater.idle()
