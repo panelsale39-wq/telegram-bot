9247f3e94d62e6f8beec1026e707ea32b03bfa5c
panelsale39-wq <panelsale39@gmail.com>
2026-03-16 13:13:23 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,52 +1,32 @@
-import re
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+from telegram.ext import Updater, CommandHandler
 
-TOKEN = "8690887958:AAEHfy8T9gH1Tc4sQnI1MDvbr8zo2oXMjfc"
+TOKEN = os.environ.get("8690887958:AAEHfy8T9gH1Tc4sQnI1MDvbr8zo2oXMjfc")
 
-total_in = 0
-total_out = 0
+balance_in = 0
+balance_out = 0
 
 def start(update, context):
-    update.message.reply_text("Balance Bot Online ✅")
+    update.message.reply_text("Bot chal raha hai ✅")
 
-def check_payment(update, context):
-    global total_in
+def add(update, context):
+    global balance_in
+    amount = int(context.args[0])
+    balance_in += amount
+    update.message.reply_text(f"You received ${amount}\nTotal In: ${balance_in}")
 
-    text = update.message.text.lower()
-
-    # amount detect
-    match = re.search(r'\$?(\d+(\.\d+)?)', text)
-
-    if "received" in text and match:
-        amount = float(match.group(1))
-        total_in += amount
-
-        update.message.reply_text(
-            f"🟢 Payment Received\n\n"
-            f"Amount: ${amount}\n"
-            f"➕ Total In: ${total_in}\n"
-            f"➖ Total Out: ${total_out}"
-        )
-
-def add_out(update, context):
-    global total_out
-
-    amount = float(context.args[0])
-    total_out += amount
-
-    update.message.reply_text(
-        f"🔴 Payment Sent\n\n"
-        f"Amount: ${amount}\n"
-        f"➕ Total In: ${total_in}\n"
-        f"➖ Total Out: ${total_out}"
-    )
+def out(update, context):
+    global balance_out
+    amount = int(context.args[0])
+    balance_out += amount
+    update.message.reply_text(f"You sent ${amount}\nTotal Out: ${balance_out}")
 
 updater = Updater(TOKEN, use_context=True)
-dp = updater.dispatcher
 
+dp = updater.dispatcher
 dp.add_handler(CommandHandler("start", start))
-dp.add_handler(CommandHandler("out", add_out))
-dp.add_handler(MessageHandler(Filters.text & ~Filters.command, check_payment))
+dp.add_handler(CommandHandler("add", add))
+dp.add_handler(CommandHandler("out", out))
 
 updater.start_polling()
 updater.idle()
